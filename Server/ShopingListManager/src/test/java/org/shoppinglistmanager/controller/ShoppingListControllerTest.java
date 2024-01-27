c71c01b0f828f219ac5cdd1fd3570c92f89e9559
nivblue <nivniv568@gmail.com>
2024-01-27 22:10:00 +0200

    Created test files

---
+++ Server/ShopingListManager/src/test/java/org/shoppinglistmanager/controller/ShoppingListControllerTest.java
@@ -0,0 +1,14 @@
+package org.shoppinglistmanager.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ShoppingListControllerTest {
+
+    @Test
+    @DisplayName("First test...")
+    void testA() {
+        Assertions.assertTrue(true);
+    }
+}
---
+++ Server/ShopingListManager/src/test/java/org/shoppinglistmanager/service/ShoppingListServiceTest.java
@@ -0,0 +1,13 @@
+package org.shoppinglistmanager.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ShoppingListServiceTest {
+    @Test
+    @DisplayName("First test...")
+    void testA() {
+        Assertions.assertTrue(true);
+    }
+}
