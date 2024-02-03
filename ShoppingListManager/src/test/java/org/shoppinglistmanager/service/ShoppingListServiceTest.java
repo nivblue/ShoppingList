2fee3e24524985213ce418a7b4bcccc2746ca5a0
nivblue <nivniv568@gmail.com>
2024-02-03 21:26:54 +0200

    Made Service test work

--- ShoppingListManager/src/test/java/org/shoppinglistmanager/service/ShoppingListServiceTest.java
+++ ShoppingListManager/src/test/java/org/shoppinglistmanager/service/ShoppingListServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.shoppinglistmanager.convertor.EntityConvertor;
+import org.shoppinglistmanager.convertor.RestConvertor;
 import org.shoppinglistmanager.entity.ShoppingList;
 import org.shoppinglistmanager.repository.ShoppingListRepository;
 import org.shoppinglistmanager.utils.AssertionsUtils;
@@ -27,17 +28,13 @@ public class ShoppingListServiceTest {
     @Test
     @DisplayName("Check get shopping list by existing id retrieve the right shopping list")
     void testGetShoppingListByExistingIdRetrievesRightList() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("hello");
-
-
         int id = 1;
-        ShoppingListRest expectedShoppingListRest = TestUtils.createShoppingListRest(id);
-        ShoppingList expectedShoppingList = EntityConvertor.convertToSql(expectedShoppingListRest);
+        ShoppingList expectedShoppingList = TestUtils.createShoppingList(id);
+        ShoppingListRest expectedShoppingListRest = RestConvertor.convertToRest(expectedShoppingList);
 
-        when(shoppingListRepository.findById(1)).thenReturn(Optional.of(expectedShoppingList));
+        when(shoppingListRepository.findById(id)).thenReturn(Optional.of(expectedShoppingList));
 
-        ShoppingListRest actualShoppingListRest = shoppingListService.getShoppingListById(1).getBody();
+        ShoppingListRest actualShoppingListRest = shoppingListService.getShoppingListById(id).getBody();
 
         AssertionsUtils.AssertShoppingListRest(actualShoppingListRest, expectedShoppingListRest);
     }
